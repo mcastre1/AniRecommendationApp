2bc48fd12c8e826aaec02ae038fd6c4053f64a27
mcastre1 <miguelcastrejongal@gmail.com>
2026-08-18 10:19:54 -0600

    Added comments to MainWindow

--- frontend/ui/MainWindow.py
+++ frontend/ui/MainWindow.py
@@ -5,15 +5,23 @@ class MainWindow(QMainWindow):
     def __init__(self, initial_data):
         super().__init__()
         self.setWindowTitle('Anime Recommendation')
-        print(initial_data)
         
+        # QMainWindow requires a centralWidget
         container = QWidget()
         
+        # Layout for container
         layout = QVBoxLayout(container)
+        
+        # We create the cards
         self.card = Card('1', 'title test', 'img test')
+        
+        # Add cards to the layout
         layout.addWidget(self.card)
         
+        # Set the layout on container
         container.setLayout(layout)
+        
+        # Set container as centralwidget
         self.setCentralWidget(container)
         
         
